1ff0d481c38ea21e2fcc31f852317f1f009946a2
Rodney Lab <codesign@rodneylab.com>
2021-06-10 11:59:00 +0100

    fix(routes/slug.json): 🔧 improved code robustness

--- src/routes/[slug].json.js
+++ src/routes/[slug].json.js
@@ -7,7 +7,11 @@ export async function get({ params }) {
 	const __dirname = path.resolve();
 	const location = path.join(__dirname, 'src/content/blog');
 	const articles = await getPostsContent(location);
-	let article = articles.find((element) => element.slug === slug);
+	const article = articles.find((element) => element.slug === slug);
+	const post = await getPost(article.content, true);
 
-	return { body: JSON.stringify({ post: await getPost(article.content, true) }) };
+	if (!post) {
+		return { body: ''};
+	}
+	return { body: JSON.stringify({ post }) };
 }
