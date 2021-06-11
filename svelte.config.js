ab1b4c54c24c3ab58ddccf66f26773106d3f02f8
Rodney Lab <codesign@rodneylab.com>
2021-06-11 19:31:28 +0100

    fix: 🔥 optimised scss prepend data only adding varaibles

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ import preprocess from 'svelte-preprocess';
 const config = {
   preprocess: preprocess({
     scss: {
-      prependData: "@import 'src/lib/styles/styles.scss';",
+      prependData: "@import 'src/lib/styles/variables.scss';",
     },
   }),
   kit: {
