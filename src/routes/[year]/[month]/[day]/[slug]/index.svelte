c7d8fee5f688e70c5e58ae32059dd95bac7264c5
Rodney Lab <codesign@rodneylab.com>
2021-06-25 18:26:54 +0100

    ci: 🚀 initial commit

---
+++ src/routes/[year]/[month]/[day]/[slug]/index.svelte
@@ -0,0 +1,14 @@
+<script context="module">
+  export async function load({ page }) {
+    const { slug } = page.params;
+    return {
+			status: 301, // can return 308 instead if that's a better match
+			redirect: `/${slug}` };
+  }
+</script>
+
+
+localhost:3030/2021/06/25/folding-camera
+
+
+http://localhost:3030/folding-camera
