6bb88e7608ca705ea83898f77443757652a228e3
Rodney Lab <codesign@rodneylab.com>
2021-07-05 09:54:37 +0100

    feat(error): ⛔️ added default 404 page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+  export function load({ error, status }) {
+    return {
+      props: {
+        title: `${status}: ${error.message}`,
+      },
+    };
+  }
+</script>
+
+<script>
+  export let title;
+</script>
+
+<svelte:head>
+  <title>{title}</title>
+</svelte:head>
+<h1>{title}</h1>
