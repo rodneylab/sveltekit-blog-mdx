464e6fa0f70d6bdc7651ecd9a212c0393242af2a
Rodney Lab <codesign@rodneylab.com>
2022-01-15 13:26:54 +0000

    fix: 💫 update copyright information

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -19,7 +19,7 @@
       href="https://rodneylab.com/"
       target="_blank"
       rel="noopener noreferrer">Rodney Lab</a
-    >. Copyright {COPYRIGHT_ENTITY} 2022.
+    >. Copyright {COPYRIGHT_ENTITY} 2021–2022.
   </div>
   <nav class="footer-icons">
     <ul>
