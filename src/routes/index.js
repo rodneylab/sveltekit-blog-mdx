a2f100fbc412e09a8dacdf3ed2f1cf9bacf1b2db
Rodney Lab <codesign@rodneylab.com>
2022-06-22 11:18:44 +0100

    style: 🛁  linting

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,6 @@ export async function get() {
   const mdModules = await import.meta.glob('../content/blog/**/index.md');
   const posts = await Promise.all(
     Object.keys(mdModules).map(async (path) => {
-      mdModules[path]();
       const slug = path.split('/').at(-2);
       const { metadata } = await mdModules[path]();
       const { datePublished, lastUpdated, postTitle, seoMetaDescription } = metadata;
