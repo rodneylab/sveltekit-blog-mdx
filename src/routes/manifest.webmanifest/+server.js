e01b5aedebedc062d5e0772cd613bf8811d96aa8
Rodney Lab <codesign@rodneylab.com>
2022-09-02 07:51:13 +0100

    fix: 💫 update web manifest generation

---
+++ src/routes/manifest.webmanifest/+server.js
@@ -0,0 +1,26 @@
+import website from '$lib/config/website';
+
+export const prerender = true;
+
+export const GET = function get({ setHeaders }) {
+	const { backgroundColor, siteShortTitle, siteTitle, themeColor } = website;
+
+	const manifest = {
+		name: siteTitle,
+		short_name: siteShortTitle,
+		start_url: '/',
+		background_color: backgroundColor,
+		theme_color: themeColor,
+		display: 'standalone',
+		icons: [
+			{ src: '/icon-192.png', type: 'image/png', sizes: '192x192' },
+			{ src: '/icon-512.png', type: 'image/png', sizes: '512x512' },
+		],
+	};
+
+	setHeaders({
+		'content-type': 'application/json',
+	});
+
+	return new Response(JSON.stringify(manifest));
+};
